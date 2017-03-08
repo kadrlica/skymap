da5b334fc059fcf18e3bfb798f96d88840ff9cfa
Alex Drlica-Wagner <kadrlica@fnal.gov>
2017-03-08 09:18:12 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+import sys
+import os
+try: from setuptools import setup
+except ImportError: from distutils.core import setup
+import versioneer
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(filename):
+    return open(os.path.join(here,filename)).read()
+
+setup(
+    name='skymapy',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    url='https://github.com/kadrlica/skymapy',
+    author='Alex Drlica-Wagner',
+    author_email='kadrlica@fnal.gov',
+    scripts = [],
+    install_requires=[],
+    packages=['skymapy'],
+    description="A place to hold python tools.",
+    long_description=read('README.md'),
+    platforms='any',
+    keywords='python tools',
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 2 - Pre-Alpha',
+        'Natural Language :: English',
+        'Intended Audience :: Science/Research',
+    ]
+)
