77c11b3427d8aafff8a0291861c9cd024d7a017f
Alex Drlica-Wagner <kadrlica@fnal.gov>
2020-06-15 14:19:05 -0500

    color

--- skymap/constants.py
+++ skymap/constants.py
@@ -5,6 +5,7 @@ Various constants used for plotting
 __author__ = "Alex Drlica-Wagner"
 
 import numpy as np
+from collections import OrderedDict as odict
 
 # Plotting DECam
 DECAM=1.1 # DECam radius (deg)
@@ -22,3 +23,23 @@ RA_SMC = 13.1867
 DEC_SMC = -72.8286
 RADIUS_SMC = 2.667 # semi_major axis (deg)
 
+COLORS = odict([
+    ('u','#56b4e9'),
+    ('g','#008060'),
+    ('r','#ff4000'),
+    ('i','#850000'),
+    ('z','#6600cc'),
+    ('Y','#000000'),
+])
+
+# Band colormaps
+CMAPS = odict([
+    ('none','binary'),
+    ('u','Blues'),
+    ('g','Greens'),
+    ('r','Reds'),
+    ('i','YlOrBr'),
+    ('z','RdPu'),
+    ('Y','Greys'),
+    ('VR','Greys'),
+])
